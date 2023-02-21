e9da9af6094a787125c11b032cf9c6c6fdb884aa
Borhan Uddin <68281712+wapborhan@users.noreply.github.com>
2023-02-21 20:09:42 +0600

    Update Main.js

--- src/components/pages/Main.js
+++ src/components/pages/Main.js
@@ -1,6 +1,6 @@
 import React, { Fragment } from "react";
 import Banner from "./banner/Banner";
-import Clock from "./prayertime/Clock";
+
 import PrayerTime from "./prayertime/PrayerTime";
 
 export default function Main(props) {
